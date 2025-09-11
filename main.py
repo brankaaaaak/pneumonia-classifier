cc436ebcdbd4878f46e22bfdfedf244f1763c4bf
unknown <kovacevicbranka123@gmail.com>
2025-09-11 17:38:45 +0200

    [INIT] parameters

---
+++ main.py
@@ -0,0 +1,8 @@
+#parameters
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 32
+EPOCHS = 10
+DATA_DIR = r'C:\Users\User\Desktop\archive\chest_xray' #path to the data
+
+if __name__ == "__main__":
+    print("Hello world")
