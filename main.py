76bd9a62b7cf42a82b776ecc5859640ab2dfe4cb
unknown <kovacevicbranka123@gmail.com>
2025-09-18 13:20:26 +0200

    [ADD] train model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from tensorflow.keras import layers, models
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 
 #parameters
@@ -61,27 +62,52 @@ def build_cnn(image_size):
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
+
+def train_model(model, train_gen, val_gen, epochs):
+    callbacks = [
+        EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True),
+        ModelCheckpoint('best_pneumonia_model.h5', save_best_only=True)
+    ]
+
+    history = model.fit(
+        train_gen,
+        epochs=epochs,
+        validation_data=val_gen,
+        callbacks=callbacks
+    )
+    return history
+
+
 if __name__ == "__main__":
-    print("Hello world")
     train_gen, val_gen, test_gen = load_data(DATA_DIR, IMAGE_SIZE, BATCH_SIZE)
-    print("Train samples:", train_gen.samples)
-    print("Validation samples:", val_gen.samples)
-    print("Test samples:", test_gen.samples)
-    print("Classes:", train_gen.class_indices)
 
-    #visualization
-    x_batch, y_batch = next(train_gen)
-    print("Batch shape:", x_batch.shape)
-    print("Labels shape:", y_batch.shape)
-
-    plt.figure(figsize=(10, 6))
-    for i in range(9):
-        plt.subplot(3, 3, i+1)
-        plt.imshow(x_batch[i])
-        plt.title("PNEUMONIA" if y_batch[i] == 1 else "NORMAL")
-        plt.axis("off")
-    plt.show()
-
-    #model creation
+    # model creation
     model = build_cnn(IMAGE_SIZE)
     model.summary()
+
+    # train model
+    history = train_model(model, train_gen, val_gen, EPOCHS)
+
+    # graph of accuracy and loss over epochs
+    plt.figure(figsize=(12,5))
+
+    # accuracy
+    plt.subplot(1,2,1)
+    plt.plot(history.history['accuracy'], label='Train Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy over epochs')
+    plt.legend()
+
+    # loss
+    plt.subplot(1,2,2)
+    plt.plot(history.history['loss'], label='Train Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Loss over epochs')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
