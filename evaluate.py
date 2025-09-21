aca6899d2308157c5f066bb317bb25ebacd3bbc9
unknown <kovacevicbranka123@gmail.com>
2025-09-21 10:54:00 +0200

    [ADD] evaluate

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,10 @@
 from tensorflow.keras.models import load_model
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+IMAGE_SIZE = (224, 224)
+BATCH_SIZE = 32
+DATA_DIR = r'C:\Users\User\Desktop\archive\chest_xray_split'
 
 def load_trained_model(model_path="best_pneumonia_model.h5"):
     """
@@ -12,3 +18,15 @@ def load_trained_model(model_path="best_pneumonia_model.h5"):
 
 if __name__ == "__main__":
     model = load_trained_model("best_pneumonia_model.h5")
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_gen = test_datagen.flow_from_directory(
+        os.path.join(DATA_DIR, "test"),
+        target_size=IMAGE_SIZE,
+        batch_size=BATCH_SIZE,
+        class_mode="binary",
+        shuffle=False
+    )
+
+    # evaluation
+    test_loss, test_acc = model.evaluate(test_gen)
+    print(f"📊 Test Accuracy: {test_acc:.4f}, Test Loss: {test_loss:.4f}")
