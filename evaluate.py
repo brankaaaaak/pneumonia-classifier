d8570a111e8473b5e4d46f76c96e2c92b16253b5
unknown <kovacevicbranka123@gmail.com>
2025-09-21 21:24:20 +0200

    [ADD] confusion matrix in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,9 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 IMAGE_SIZE = (224, 224)
 BATCH_SIZE = 32
@@ -33,11 +36,22 @@ if __name__ == "__main__":
     test_loss, test_acc = model.evaluate(test_gen)
     print(f"📊 Test Accuracy: {test_acc:.4f}, Test Loss: {test_loss:.4f}")
 
-    # === predictions on the test set ===
+    # predictions on the test set 
     y_pred = model.predict(test_gen)
     y_pred_classes = (y_pred > 0.5).astype("int32").flatten()
     y_true = test_gen.classes
 
-    # === classification report ===
+    # classification report
     print("\n📊 Classification Report:")
     print(classification_report(y_true, y_pred_classes, target_names=list(test_gen.class_indices.keys())))
+
+    # confusion matrix
+    cm = confusion_matrix(y_true, y_pred_classes)
+    plt.figure(figsize=(5,4))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+                xticklabels=test_gen.class_indices.keys(),
+                yticklabels=test_gen.class_indices.keys())
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.title("Confusion Matrix")
+    plt.show()
